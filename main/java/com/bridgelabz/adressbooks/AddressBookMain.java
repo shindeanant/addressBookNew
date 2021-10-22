f5ebb837df6cabe93ea6feec30647697e95cb1e2
shindeanant <shindeanant1997@gmail.com>
2021-10-22 12:06:56 +0530

    Added: Ability to create contact

---
+++ main/java/com/bridgelabz/adressbooks/AddressBookMain.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.adressbooks;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println(
+				"***********************************************\n          Welcome To Address Book\n***********************************************\n");
+		AddressBookMain appAddressBookMain = new AddressBookMain();
+		System.out.println(appAddressBookMain.createContact());
+	}
+
+	private Contacts createContact() {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter First Name: ");
+		String firstName = scanner.nextLine();
+
+		System.out.print("Enter Last Name: ");
+		String lastName = scanner.nextLine();
+
+		System.out.print("Enter Address: ");
+		String address = scanner.nextLine();
+
+		System.out.print("Enter City: ");
+		String city = scanner.nextLine();
+
+		System.out.print("Enter State: ");
+		String state = scanner.nextLine();
+
+		System.out.print("Enter ZipCode: ");
+		String zipcode = scanner.nextLine();
+
+		System.out.print("Enter Phone Number: ");
+		String number = scanner.nextLine();
+
+		System.out.print("Enter Email id: ");
+		String email = scanner.nextLine();
+
+		return new Contacts(firstName, lastName, address, city, state, zipcode, number, email);
+
+	}
+
+}
